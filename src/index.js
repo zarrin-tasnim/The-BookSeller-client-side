feb8764145239e0d3c8d6def2058b0dd38a4cf93
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-11-27 19:01:40 +0600

    favicon added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'react-day-picker/dist/style.css';
+// import AuthProvider from './contexts/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+
+
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      
+        <App />
+      
+    </QueryClientProvider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
