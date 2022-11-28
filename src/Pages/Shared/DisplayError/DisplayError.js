71a76b22f6ebda19069157c0a283715da6e10ceb
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-11-28 13:54:04 +0600

    error mg fixed

--- src/Pages/Shared/DisplayError/DisplayError.js
+++ src/Pages/Shared/DisplayError/DisplayError.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useNavigate, useRouteError } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider';
+import errorMsg from '../../../assets/images/error.jpg';
 
 const DisplayError = () => {
     const { logOut } = useContext(AuthContext);
@@ -16,10 +17,10 @@ const DisplayError = () => {
     }
 
     return (
-        <div>
-            
-            <p className='text-red-500'>Something went wrong!!!</p>
-            <p className='text-red-400'>{error.statusText || error.message}</p>
+        <div className='grid grid-cols-1 mx-40 my-20'>
+            <img className='flex justify-center' src={errorMsg} alt="" />
+            <p className='text-red-500 text-2xl'>Something went wrong!!!</p>
+            <p className='text-red-400 text-4xl'>{error.statusText || error.message}</p>
             <h4 className="text-3xl"> Please <button onClick={handleLogOut}>Sign out</button> and log back in</h4>
         </div>
     );
